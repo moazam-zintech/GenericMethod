1e94f6c195af53d757d306becf6945fbb6c95399
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-07-01 14:32:31 +0500

    Add project files.

---
+++ GenericMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string a = "3", b = "5";
+        Console.WriteLine("test");
+        Swap1<string>(ref a,ref b);
+        Console.WriteLine(a+ ","+ b);
+    }
+    //This function is non generic as it does not supported other datatypes of refrence except integer
+     static void Swap(ref int x, ref int y)
+    {
+        int temp=x; x=y; y=temp; y=temp;
+    }
+    //Swap is now Generic class IT will accept any type of Data
+    static void Swap1<T>(ref T x, ref T y)
+    {
+        T temp = x; x = y; y = temp; y = temp;
+    }
+}
